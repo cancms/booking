49e07e2c4f8390fb1437dd1670182ad831f93774
tt2021 <Ft@qwe12>
2022-11-25 16:22:54 +0800

    user manual

---
+++ user-manual/assets/index.md.999358af.js
@@ -0,0 +1,1 @@
+import{f as e,c as t,o as a}from"./app.a104fce0.js";const i=JSON.parse('{"title":"","description":"","frontmatter":{"home":false},"headers":[],"relativePath":"index.md"}'),o={name:"index.md"},d=Object.assign(o,{setup(n){return e(()=>{window.location.href="/guide/"}),(r,s)=>(a(),t("div"))}});export{i as __pageData,d as default};
